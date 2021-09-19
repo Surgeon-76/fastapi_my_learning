f4047c97d979f4835e4fde238509d5d2650f7b7b
Surgeon76 <Surgeon76@yandex.ru>
2021-09-19 20:35:10 +0300

    2

--- app_shop/schemas.py
+++ app_shop/schemas.py
@@ -4,6 +4,65 @@ from typing import List
 from pydantic import BaseModel
 
 
+# ########################################## Товар
+# class ItemBase(BaseModel):
+#     name: str
+#     cost_price: float
+#     selling_price: float
+#     quantity: int
+
+
+# class ItemCreate(ItemBase):
+#     pass
+
+
+# class Item(ItemBase):
+#     id: int
+
+
+#     class Config:
+#         orm_mode = True
+
+
+########################################### Заказы(дата)
+class OrderBase(BaseModel):
+    date_placed: datetime
+
+
+class OrderCreate(OrderBase):
+    pass
+
+
+class Order(OrderBase):
+    id: int
+    customer_id: int
+    #line_items: List["OrderItem"] = []
+
+
+    class Config:
+        orm_mode = True
+
+
+# ########################################### Заказ - Товар(кол-во)
+# class OrderItemBase(BaseModel):
+#     quantity: int
+
+
+# class OrderItemCreate(OrderItemBase):
+#     pass
+
+
+# class OrderItem(OrderItemBase):
+#     id: int
+#     order_id: int
+#     item_id: int
+#     item: List[Item] = []
+
+
+#     class Config:
+#         orm_mode = True
+
+
 ########################################## Покупатели
 class CustomerBase(BaseModel):
     first_name: str
@@ -20,67 +79,8 @@ class CustomerCreate(CustomerBase):
 
 class Customer(CustomerBase):
     id: int
-    orders: List["Order"] = []
+    orders: List[Order] = []
 
 
     class Config:
         orm_mode = True
-
-########################################## Товар
-class ItemBase(BaseModel):
-    name: str
-    cost_price: float
-    selling_price: float
-    quantity: int
-
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    id: int
-
-
-    class Config:
-        orm_mode = True
-
-
-########################################### Заказы(дата)
-class OrderBase(BaseModel):
-    date_placed: datetime
-
-
-class OrderCreate(OrderBase):
-    pass
-
-
-class Order(OrderBase):
-    id: int
-    customer_id: int
-    line_items: List["OrderItem"] = []
-
-
-    class Config:
-        orm_mode = True
-
-
-########################################### Заказ - Товар(кол-во)
-class OrderItemBase(BaseModel):
-    quantity: int
-
-
-class OrderItemCreate(OrderItemBase):
-    pass
-
-
-class OrderItem(OrderItemBase):
-    id: int
-    order_id: int
-    item_id: int
-    item: List[Item] = []
-
-
-    class Config:
-        orm_mode = True
-
